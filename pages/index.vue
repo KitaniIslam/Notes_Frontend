d68b45fcfebe47310036e8e7cd9fca7a0e087763
KitaniIslam <slmkitani@gmail.com>
2020-03-18 19:07:34 +0100

    LandingPage structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,62 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        corona_fighters
-      </h1>
-      <h2 class="subtitle">
-        My polished Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+    <a-row class="container">
+      <a-col :span="12" class="left-container">
+        <login />
+      </a-col>
+      <a-col :span="12" class="right-container">
+        <h1 class="yellow-txt">WE SURVIVE TOGETHER</h1>
+        <p class="orang34495e-txt">Community that fight corona together for a better tomorrow.</p>
+      </a-col>
+    </a-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Login from '~/components/Login.vue'
 
 export default {
   components: {
-    Logo
+    Login
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .left-container {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: #eeeff0;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .right-container {
+    background-image: url('../static/images/mask2.jpg');
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .yellow-txt {
+    color: #34495e;
+  }
+
+  .orang-txt {
+    color: #34495e;
+  }
+
+  h1 {
+    font-size: 36px;
+    margin: 0;
+  }
+
 </style>
