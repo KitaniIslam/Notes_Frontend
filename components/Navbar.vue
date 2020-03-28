df758eacef8487f28150f278f740180933b81639
KitaniIslam <slmkitani@gmail.com>
2020-03-28 02:36:58 +0100

    formating

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,9 @@
 <template>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-        theme='dark'
-        mode="horizontal"
-        :defaultSelectedKeys="['1']"
-        :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">Home</a-menu-item>
-        <a-menu-item key="2">About</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu theme='dark' mode="horizontal" :defaultSelectedKeys="['1']" :style="{ lineHeight: '64px' }">
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">About</a-menu-item>
+    </a-menu>
+  </a-layout-header>
 </template>
