8be3d526847ede2bb42cf128c4a7bd702563b784
KitaniIslam <slmkitani@gmail.com>
2020-03-28 23:01:20 +0100

    set user store

---
+++ store/user.js
@@ -0,0 +1,48 @@
+function getName(row) {
+    const name = row.split('@');
+    return name[0];
+  }
+  
+  export const state = () => ({
+    user: {
+      name: '',
+      email: ''
+    },
+    notes: [],
+    filter: 'all',
+    dataToRender: []
+  });
+  
+  export const mutation = {
+    setUser(state, payload) {
+      state.user.email = payload.email;
+      state.notes = payload.notes;
+      state.dataToRender = payload.notes;
+      state.user.name = getName(payload.email);
+    },
+    apllyFilter(state, payload) {
+      if (payload.filter == 'all') {
+        state.dataToRender = state.notes;
+      } else {
+        state.dataToRender = state.notes.filter(item => {
+          return item.category.name == payload.filter;
+        });
+      }
+    }
+  };
+  
+  export const getters = {
+    getNotes: (state) => {
+      return state.dataToRender;
+    },
+    getName: (state) => {
+      return state.user.name;
+    },
+    getEmail: (state) => {
+      return state.user.email;
+    },
+    getCurrentFilter: (state) => {
+      return state.filter;
+    }
+  };
+  
