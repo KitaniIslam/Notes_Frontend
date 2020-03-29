32fe4ffa81f58d1689bc0476fe925c60262e9d65
KitaniIslam <slmkitani@gmail.com>
2020-03-29 04:56:31 +0100

    number of cards for each row: 3

--- components/List.vue
+++ components/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-list :grid="{ md: 2 ,gutter: 16}"  :dataSource="notes">
+  <a-list :grid="{ md: 3 ,gutter: 16}"  :dataSource="notes">
     <a-list-item slot="renderItem" slot-scope="item,index">
       <note-card :simple="item"/>
     </a-list-item>
