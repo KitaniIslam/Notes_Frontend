ad36f5d06c1c578a37c42191c6016d304c6b1aec
KitaniIslam <slmkitani@gmail.com>
2020-03-29 04:56:44 +0100

    navbar styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,59 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
-    <a-menu theme='dark' mode="horizontal" :defaultSelectedKeys="['1']" :style="{ lineHeight: '64px' }">
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">About</a-menu-item>
-    </a-menu>
+    <div class="action">
+      <nuxt-link to="/">
+        <a-button type="link"><p>Home</p></a-button>
+      </nuxt-link>
+      <nuxt-link to="/about">
+        <a-button type="link" disabled><p>About</p></a-button>
+      </nuxt-link>
+      <div class="logo" />
+      <a-button type="link" disabled=""><p>slmkitani</p></a-button>
+      <nuxt-link to="">
+      <a-button type="link"><p @click="logout()">Logout</p></a-button>
+
+      </nuxt-link>
+    </div>
   </a-layout-header>
 </template>
+
+<script>
+export default {
+  methods: {
+    logout(){
+      this.$auth.logout();
+      this.$router.push('/login');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .header {
+    background-color: #CBC7C4;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .action {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+  }
+  p {
+    margin: 0;
+    color: #242425;
+  }
+
+  .logo {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 20px;
+    margin-left: 20px;
+    background-image: url('../assets/pictures/my_logo.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+</style>
